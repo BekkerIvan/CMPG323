408c37b3c1f370eac1574d042cdcbd9605ba1c14
ivan.bekker@stratusolce.com <ivan.bekker@stratusolve.com>
2021-09-01 21:00:26 +0200

    Init Commit

---
+++ src/test/java/com/cmpg/nwu/accsystem/AccSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpg.nwu.accsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
